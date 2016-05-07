50b11dd7a0410f712c7478f9e7debd64627265b4
Robert Norris <rob@eatenbyagrue.org>
2016-05-07 22:39:43 +1000

    Challenge generator

--- lib/Authen/U2F.pm
+++ lib/Authen/U2F.pm
@@ -2,7 +2,27 @@ package Authen::U2F;
 
 # ABSTRACT: FIDO U2F library
 
+use 5.010;
 use warnings;
 use strict;
+
+use namespace::sweep;
+
+use Types::Standard -types;
+use Type::Params qw(compile);
+
+use Math::Random::Secure qw(irand);
+use MIME::Base64 qw(encode_base64);
+
+sub generate_challenge {
+  state $check = compile(
+    ClassName,
+  );
+  my ($class) = $check->(@_);
+
+  my $raw = pack "L*", map { irand } 1..8;
+  my $challenge = encode_base64url($raw);
+  return $challenge;
+}
 
 1;
